56ccd6725703f0113b35daf2f2b584ad35e0d8a0
jonty Trombik <jat157@uclive.ac.nz>
2016-02-29 16:33:55 +1300

    test commit. Added simple JFrame window.

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -1,9 +1,26 @@
 package seng302;
 
-public class App 
+import javax.swing.*;
+import java.awt.*;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Welcome to SENG302" );
+
+        //test window
+
+        JFrame window = new JFrame("Allegro");
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setSize(800,600);
+        window.setPreferredSize(new Dimension(800,600));
+
+        window.getContentPane().add(new JLabel("Enter command: "));
+        //window.getContentPane().add(new JTextField());
+        window.pack();
+        window.setVisible(true);
     }
 }
