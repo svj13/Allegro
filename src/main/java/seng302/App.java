86908eedafe990cbeb5c22f50a8ee7c3a0b62905
jonty Trombik <jat157@uclive.ac.nz>
2016-02-29 16:40:33 +1300

    Reverted last commit.
    Was meant to commit to the development branch, so have removed the changes I made.

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -1,7 +1,6 @@
 package seng302;
 
-import javax.swing.*;
-import java.awt.*;
+
 
 public class App
 {
@@ -9,18 +8,5 @@ public class App
     {
         System.out.println( "Welcome to SENG302" );
 
-        //test window
-
-        JFrame window = new JFrame("Allegro");
-
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        window.setSize(800,600);
-        window.setPreferredSize(new Dimension(800,600));
-
-        window.getContentPane().add(new JLabel("Enter command: "));
-        //window.getContentPane().add(new JTextField());
-        window.pack();
-        window.setVisible(true);
     }
 }
