a3afa4615b0c3354f9c7a3943a086bd5531bb799
Dominic Jarvis <djj36@uclive.ac.nz>
2016-03-02 13:40:02 +1300

    Implement note class
    Simple functionality at the moment.
    An object to contain information about a specific note, including the corresponding midi value and appropriate functionality.
    story:97 pair[iet11, djj36]

---
+++ src/main/java/seng302/objects/Note.java
@@ -0,0 +1,21 @@
+package seng302.objects;
+
+/**
+ * Created by jat157 on 2/03/16.
+ */
+public class Note {
+    int midi;
+    String note;
+
+    public Note(int midi, String note){
+        this.midi = midi;
+        this.note = note;
+
+    }
+
+//    public  getSemitoneHigher(){
+//
+//    }
+
+
+}
