6e07f3fbde561305ee806ddd9e8d7c633807d9f7
Isabelle Taylor <iet11@uclive.ac.nz>
2016-03-04 11:01:42 +1300

    Added a function to add a 4 to the note name if no octave is specified. story:97

--- src/main/java/seng302/command/Midi.java
+++ src/main/java/seng302/command/Midi.java
@@ -7,13 +7,24 @@ import seng302.data.Notes;
  * Created by emily on 2/03/16.
  */
 public class Midi implements Command {
-    private String midi;
+    private String s;
     Notes notes;
     public Midi(String s) {
-        midi = s;
+        this.s = addDefaultOctave(s);
         notes = new Notes();
     }
     public void execute(Environment env) {
-        env.println(Integer.toString(notes.getMidi(midi)));
+        env.println(Integer.toString(notes.getMidi(s)));
     }
+
+    private String addDefaultOctave(String s){
+        try{
+            String last = s.substring(s.length() - 1);
+            Integer.valueOf(last);
+        }
+        catch(Exception e) {
+            s = s + "4";
+        }
+        return s;
+    };
 }
