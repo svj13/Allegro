8f1b2bbaa981f0c1b6e307080acb85921770b680
Emily Price <erp35@uclive.ac.nz>
2016-03-04 14:11:08 +1300

    Example Test
    Tests that the note command acts as expected.
    Story: 97

---
+++ src/test/java/seng302/command/NoteCommandTest.java
@@ -0,0 +1,28 @@
+package seng302.command;
+
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import seng302.Environment;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NoteCommandTest {
+
+    @Mock private Environment env;
+
+    @Test
+    public void printsCorrectNote() {
+        new NoteCommand(1).execute(env);
+        verify(env).println("C#-1");
+    }
+
+    @Test
+    public void printsCorrectError() {
+        new NoteCommand(128).execute(env);
+        verify(env).println("[ERROR] The provided number is not a valid MIDI value.");
+    }
+}
