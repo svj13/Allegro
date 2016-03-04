1026fc8767aa807b8c0855b96ca926df946a6e68
Joseph Weaver <jmw280@uclive.ac.nz>
2016-03-05 11:29:04 +1300

    I re added the Tuple class because it hadnt been added to git correctly   Story : 96

---
+++ src/main/java/seng302/utility/Tuple.java
@@ -0,0 +1,35 @@
+package seng302.utility;
+
+/**
+ * Created by Joseph on 5/03/2016.
+ */
+public class Tuple {
+
+    private String command;
+    private String result;
+
+
+    Tuple(String commandString){
+        command = commandString;
+    }
+
+    Tuple(String commandString, String resultString){
+        command = commandString;
+        result = resultString;
+    }
+
+
+
+    public String getCommand(){
+        return command;
+    }
+
+    public String getResult(){
+        return result;
+    }
+
+    public void setResult(String str){
+        result = str;
+    }
+
+}
