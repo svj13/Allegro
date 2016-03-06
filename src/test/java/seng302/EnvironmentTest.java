080473a37d7f946d1e93b612d6c9e64d5c1de309
Isabelle Taylor <iet11@uclive.ac.nz>
2016-03-07 09:49:37 +1300

    Tests for Environment Class.
     story:97

---
+++ src/test/java/seng302/EnvironmentTest.java
@@ -0,0 +1,35 @@
+package seng302;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import seng302.utility.TranscriptManager;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by isabelle on 7/03/16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class EnvironmentTest {
+
+    private Environment env;
+    @Mock
+    private TranscriptManager transcriptManager;
+
+    @Before
+    public void setUp() throws Exception {
+        env = new Environment();
+        env.setTranscriptManager(transcriptManager);
+    }
+
+    @Test
+    public void testError() throws Exception {
+        env.error("This went wrong.");
+        verify(transcriptManager).setCommand("[ERROR] This went wrong.\n");
+    }
+}
