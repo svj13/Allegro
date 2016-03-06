5a989d42181796a0bc5d17f2ec191c060dd492b6
Emily Price <erp35@uclive.ac.nz>
2016-03-06 17:04:24 +1300

    Midi Command Test
    Tests that the midi command acts as expected. Tests for both 'good' and 'bad' input.
    Story: 97

---
+++ src/test/java/seng302/command/MidiTest.java
@@ -0,0 +1,42 @@
+package seng302.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import seng302.Environment;
+import seng302.utility.TranscriptManager;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MidiTest {
+
+    private Environment env;
+    @Mock private TranscriptManager transcriptManager;
+
+    @Before
+    public void setUp() throws Exception {
+        env = new Environment();
+        env.setTranscriptManager(transcriptManager);
+    }
+
+    @Test
+    public void printsCorrectMidi() {
+        new Midi("C#-1").execute(env);
+        verify(transcriptManager).setResult("1");
+    }
+
+    @Test
+    public void printsCorrectMidiUnspecifiedOctave() {
+        new Midi("C#").execute(env);
+        verify(transcriptManager).setResult("61");
+    }
+
+    @Test
+    public void setsCorrectErrorResult() {
+        new Midi("Z#").execute(env);
+        verify(transcriptManager).setResult("Invalid note");
+    }
+}
