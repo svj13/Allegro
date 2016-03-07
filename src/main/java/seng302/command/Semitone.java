8193c6b3ad62f0c943e96c89c54feb21864ef7ea
Emily Price <erp35@uclive.ac.nz>
2016-03-07 14:13:18 +1300

    Fixed Bug with Semitone Command
    Semitone class was missing error checking for the case when an octave is not specified.
    Story: 98 & 99

--- src/main/java/seng302/command/Semitone.java
+++ src/main/java/seng302/command/Semitone.java
@@ -30,9 +30,15 @@ public class Semitone implements Command {
         }
         else
         {
-            this.note = Note.lookup(OctaveUtil.addDefaultOctave(searchval));
-            if(up) env.getTranscriptManager().setResult(OctaveUtil.removeOctaveSpecifier(note.semitoneUp().getNote()));
-            else env.getTranscriptManager().setResult(OctaveUtil.removeOctaveSpecifier(note.semitoneDown().getNote()));
+            try {
+                this.note = Note.lookup(OctaveUtil.addDefaultOctave(searchval));
+                if (up)
+                    env.getTranscriptManager().setResult(OctaveUtil.removeOctaveSpecifier(note.semitoneUp().getNote()));
+                else
+                    env.getTranscriptManager().setResult(OctaveUtil.removeOctaveSpecifier(note.semitoneDown().getNote()));
+            } catch (Exception e){
+                env.getTranscriptManager().setResult("Note is not contained in the midi library");
+            }
         }
     }
 }
