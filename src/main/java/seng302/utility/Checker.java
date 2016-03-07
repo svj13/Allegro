2510da58d7b8c4b33b4b59e0c2f4a20c4fcbfb3c
Emily Price <erp35@uclive.ac.nz>
2016-03-07 15:16:18 +1300

    Updated Midi Command
    Midi command now only adds a default octave if the input looks like a valid (octave-less) note. Otherwise, with error messages you get weird outputs:
    > midi blah
    "[ERROR] blah4 is not a valid note"
    Story: 97

--- src/main/java/seng302/utility/Checker.java
+++ src/main/java/seng302/utility/Checker.java
@@ -14,6 +14,10 @@ public class Checker {
         return note.matches("^[A-G|a-g]([0-8]|-1)?$|^[C|D|G|F|A]#?([0-8]|-1)?$|^[C|D|F]#?9$|^[E|G]9");
     }
 
+    public static boolean isValidNoteNoOctave(String note){
+        return note.matches("^[A-G|a-g]#?$");
+    }
+
     public static boolean isValidMidiNote(String note){
         return note.matches("^(0?[0-9]?[0-9]|1[01][0-9]|12[0-7])$");
     }
