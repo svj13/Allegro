f7b120b96b741aed11f10ab9ec2b9cfd0e06e84d
Isabelle Taylor <iet11@uclive.ac.nz>
2016-03-08 17:36:05 +1300

    Version command tests. story:97

--- src/main/java/seng302/command/Version.java
+++ src/main/java/seng302/command/Version.java
@@ -4,7 +4,17 @@ import seng302.Environment;
 
 public class Version implements Command {
 
+	private String currentVersion;
+
+    public Version(){
+        currentVersion = "1.0";
+    }
+
+    public String getCurrentVersion(){
+        return  currentVersion;
+    }
+
 	public void execute(Environment env) {
-		env.getTranscriptManager().setResult("Current Version: 0.1b");
+		env.getTranscriptManager().setResult("Current Version: " + currentVersion);
 	}
 }
---
+++ src/test/java/seng302/command/VersionTest.java
@@ -0,0 +1,36 @@
+package seng302.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import seng302.Environment;
+import seng302.utility.TranscriptManager;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by isabelle on 8/03/16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class VersionTest {
+
+    private Environment env;
+    @Mock private TranscriptManager transcriptManager;
+
+    @Before
+    public void setUp() throws Exception {
+        env = new Environment();
+        env.setTranscriptManager(transcriptManager);
+    }
+    @Test
+    public void testExecute() throws Exception {
+        Version version = new Version();
+        version.execute(env);
+        verify(transcriptManager).setResult("Current Version: " + version.getCurrentVersion());
+
+    }
+}
