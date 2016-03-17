8db9f1d20199667005b0d2e452079228df2d7331
= <jat157@uclive.ac.nz>
2016-03-18 10:07:57 +1300

    Refactor converting over to javaFX
    story:201 pair[jmw280]

--- src/main/java/seng302/gui/RootController.java
+++ src/main/java/seng302/gui/RootController.java
@@ -1,6 +1,27 @@
 package seng302.gui;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class RootController {
 
+    @FXML
+    private Button btnGo;
+
+    @FXML
+    private TextArea txtTranscript;
+
+
+    @FXML
+    private void initialize(){
+
+        txtTranscript.setText("hello");
+    }
+
+    @FXML
+    private void goAction(){
+        System.out.println("button clicked");
+    }
 }
