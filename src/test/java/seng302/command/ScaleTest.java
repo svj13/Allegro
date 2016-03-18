3e4c96578ecc71df244624d44fee5a4472d07c55
Isabelle Taylor <iet11@uclive.ac.nz>
2016-03-18 15:54:44 +1300

    So many failing Scale tests. But they can't really be implemented until enharmonics is fully implemented.
    Story: 102

--- src/test/java/seng302/command/ScaleTest.java
+++ src/test/java/seng302/command/ScaleTest.java
@@ -34,5 +34,48 @@ public class ScaleTest {
         verify(transcriptManager).setResult("C4 D4 E4 F4 G4 A4 B4 C5");
     }
 
+    @Test
+    public void setsCorrectScaleResultWithoutOctaveSpecifier() {
+        new Scale("C", "major").execute(env);
+        verify(transcriptManager).setResult("C D E F G A B C");
+    }
+
+    @Test
+    public void setsCorrectScaleResultWithFlat() {
+        new Scale("F", "major").execute(env);
+        verify(transcriptManager).setResult("F G A Bb C D E F");
+    }
+
+    @Test
+    public void setsCorrectScaleResultWithEnharmonicSharp() {
+        new Scale("F#", "major").execute(env);
+        verify(transcriptManager).setResult("F# G# A# B C# D# E# F#");
+    }
+
+    @Test
+    public void setsCorrectScaleResultWithTwoEnharmonicSharps() {
+        new Scale("C#", "major").execute(env);
+        verify(transcriptManager).setResult("C# D# E# F# G# A# B# C#");
+    }
+
+    @Test
+    public void setsCorrectScaleResultWithFlatScale() {
+        new Scale("Fb", "major").execute(env);
+        verify(transcriptManager).setResult("Fb Gb Ab Bbb Cb Db Eb Fb");
+    }
+
+    @Test
+    public void setsCorrectErrorMessage() {
+        new Scale("C4", "cake").execute(env);
+        verify(transcriptManager).setResult("[ERROR] Invalid scale type: 'cake'.");
+    }
+
+    @Test
+    public void setsCorrectErrorMessageBadNote() {
+        new Scale("cake", "major").execute(env);
+        verify(transcriptManager).setResult("[ERROR] Note is not contained in the MIDI library.");
+    }
+
+
 
 }
