86c0c6a82415dbba4d61b3744e04a03f6068590a
Sarah Jackson <svj13@uclive.ac.nz>
2016-04-01 14:30:59 +1300

    Interval class
    Did nothing with it. Just made an empty class
    
    story #108

---
+++ src/main/java/seng302/command/Interval.java
@@ -0,0 +1,15 @@
+package seng302.command;
+
+/**
+ * Created by Sarah on 1/04/2016.
+ */
+import java.util.ArrayList;
+
+import seng302.Environment;
+import seng302.data.Note;
+import seng302.utility.Checker;
+import seng302.utility.OctaveUtil;
+
+/
+public class Interval implements Command {
+}
