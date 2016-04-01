c6b6bdbdaa90209b1b1e26811194d1d467f21f58
Sarah Jackson <svj13@uclive.ac.nz>
2016-04-01 14:34:49 +1300

    Interval class
    Did nothing with it. Just made an empty class. It isn't hooked up to the dsl.flex or dsl.cup
    
    story #108

--- src/main/java/seng302/command/Interval.java
+++ src/main/java/seng302/command/Interval.java
@@ -3,6 +3,16 @@ package seng302.command;
 /**
  * Created by Sarah on 1/04/2016.
  */
+
+
+// *********************************
+//*******************************
+//CURRENTLY NOT HOOKED UP TO THE DSL CUP OR FLEX
+//**********************************
+//*****************************
+
+
+
 import java.util.ArrayList;
 
 import seng302.Environment;
@@ -10,6 +20,17 @@ import seng302.data.Note;
 import seng302.utility.Checker;
 import seng302.utility.OctaveUtil;
 
-/
+
 public class Interval implements Command {
+    String search;
+    String type;
+    String outputType;
+    private boolean octaveSpecified;
+    private Note note;
+    private char[] letters;
+
+    public void execute(Environment env){
+
+    }
 }
+
