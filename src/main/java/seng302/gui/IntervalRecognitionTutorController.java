d202655dd9475f499e3c183048c508f394a6ec67
Emily Price <erp35@uclive.ac.nz>
2016-04-08 15:07:43 +1200

    Generates correct number of questions on GUI with all required buttons. Currently has no functionality.
    Story: 110

--- src/main/java/seng302/gui/IntervalRecognitionTutorController.java
+++ src/main/java/seng302/gui/IntervalRecognitionTutorController.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class IntervalRecognitionTutorController {
@@ -32,17 +34,27 @@ public class IntervalRecognitionTutorController {
     // hash map where the key is the number of semitones and the value is the name of that interval
     private HashMap intervals = new HashMap(8);
 
+    @FXML
+    private void initialize() {
+        populateIntervals();
+    }
+
 
     @FXML
     void goAction(ActionEvent event) {
         int numberIntervals = Integer.parseInt(txtNumIntervals.getText());
         if (numberIntervals >= 1){
             // Run the tutor
-            populateIntervals();
+            questionRows.getChildren().clear();
+            for (int i = 0; i < numberIntervals; i++) {
+                HBox questionRow = generateQuestionRow();
+                questionRows.getChildren().add(questionRow);
+                questionRows.setMargin(questionRow, new Insets(10, 10, 10, 10));
+            }
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText("Invalid Number of Intervals");
-            alert.setContentText("Please select a number of intervals that is 1 or greater");
+            alert.setContentText("Please select a positive number of intervals");
             alert.setResizable(false);
             alert.showAndWait();
         }
@@ -56,7 +68,6 @@ public class IntervalRecognitionTutorController {
     }
 
 
-
     void populateIntervals() {
         intervals.put(0, "unison");
         intervals.put(2, "major second");
@@ -66,6 +77,25 @@ public class IntervalRecognitionTutorController {
         intervals.put(9, "major sixth");
         intervals.put(11, "major seventh");
         intervals.put(12, "perfect octave");
+    }
+
+    private HBox generateQuestionRow() {
+        final HBox questionRow = new HBox();
+
+        questionRow.setPadding(new Insets(10, 10, 10, 10));
+        questionRow.setSpacing(10);
+        questionRow.setStyle("-fx-background-color: #336699;");
+
+        //Add buttons for play, skip, and cancel
+        Button play = new Button("Play");
+        Button skip = new Button("Skip");
+        Button cancel = new Button("Cancel");
+        questionRow.getChildren().add(play);
+        questionRow.getChildren().add(skip);
+        questionRow.getChildren().add(cancel);
+        questionRow.getChildren().add(generateChoices());
+
+        return questionRow;
     }
 
 }
