93eedb96a2fb0a0930cb678a317385a0fd50f795
Emily Price <erp35@uclive.ac.nz>
2016-04-08 14:08:50 +1200

    Interval Tutor takes user input for number of intervals.
    Story: 110

--- src/main/java/seng302/gui/IntervalRecognitionTutorController.java
+++ src/main/java/seng302/gui/IntervalRecognitionTutorController.java
@@ -1,11 +1,45 @@
 package seng302.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class IntervalRecognitionTutorController {
 
     @FXML
+    private TextField txtNumIntervals;
+
+    @FXML
+    private VBox questionRows;
+
+    @FXML
     private AnchorPane IntervalRecognitionTab;
+
+    @FXML
+    private ScrollPane paneQuestions;
+
+    @FXML
+    private Button btnGo;
+
+
+    @FXML
+    void goAction(ActionEvent event) {
+        int numberIntervals = Integer.parseInt(txtNumIntervals.getText());
+        if (numberIntervals >= 1){
+            // Run the tutor
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Invalid Number of Intervals");
+            alert.setContentText("Please select a number of intervals that is 1 or greater");
+            alert.setResizable(false);
+            alert.showAndWait();
+        }
+
+    }
 
 }
