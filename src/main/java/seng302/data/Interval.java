2197972be1782656622576b62fee1407b0f2fb57
Emily Price <erp35@uclive.ac.nz>
2016-04-11 16:23:38 +1200

    Further refactoring.
    Story: 110

--- src/main/java/seng302/data/Interval.java
+++ src/main/java/seng302/data/Interval.java
@@ -7,14 +7,24 @@ import java.util.HashMap;
  */
 public class Interval {
 
-    public static HashMap<Integer, String> intervals = new HashMap<Integer, String>() {{
-        put(0, "unison");
-        put(2, "major second");
-        put(4, "major third");
-        put(5, "perfect fourth");
-        put(7, "perfect fifth");
-        put(9, "major sixth");
-        put(11, "major seventh");
-        put(12, "perfect octave");
+    private int semitones;
+    private String name;
+
+    protected Interval(int semitones, String name) {
+        this.semitones = semitones;
+        this.name = name;
+    }
+
+    // Populates a hashmap of intervals.
+    public static HashMap<String, Interval> intervals = new HashMap<String, Interval>() {{
+        put("unison", new Interval(0, "unison"));
+        put("major second", new Interval(2, "major second"));
+        put("major third", new Interval(4, "major third"));
+        put("perfect fourth", new Interval(5, "perfect fourth"));
+        put("perfect fifth", new Interval(7, "perfect fifth"));
+        put("major sixth", new Interval (9, "major sixth"));
+        put("major seventh", new Interval(11, "major seventh"));
+        put("perfect octave", new Interval(12, "perfect octave"));
     }};
+
 }
