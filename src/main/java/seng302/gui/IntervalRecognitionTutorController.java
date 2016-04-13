94c20a30e1c5317a6c5430a7b510862fc544082d
Emily Price <erp35@uclive.ac.nz>
2016-04-13 13:44:34 +1200

    Disables all inputs for a question after a user has picked an answer. This was allowing the user to get the right answer, and was messing with the score calculation.
    Story: 110

--- src/main/java/seng302/gui/IntervalRecognitionTutorController.java
+++ src/main/java/seng302/gui/IntervalRecognitionTutorController.java
@@ -82,6 +82,12 @@ public class IntervalRecognitionTutorController {
         return options;
     }
 
+    private void disableButtons(HBox questionRow) {
+        for (int i = 0; i < questionRow.getChildren().size(); i++) {
+            questionRow.getChildren().get(i).setDisable(true);
+        }
+    }
+
 
     /**
      * Creates a GUI section for one question.
@@ -118,10 +124,7 @@ public class IntervalRecognitionTutorController {
         skip.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 //Disables skip and cancel buttons, combo box also
-                for (int i = 0; i < questionRow.getChildren().size(); i++) {
-                    questionRow.getChildren().get(i).setDisable(true);
-                }
-
+                disableButtons(questionRow);
 
                 manager.questions -= 1;
                 manager.add(firstNote.getNote(), secondNote.getNote(), false);
@@ -144,6 +147,7 @@ public class IntervalRecognitionTutorController {
         options.setOnAction(new EventHandler<ActionEvent>() {
             // This handler colors the GUI depending on the user's input
             public void handle(ActionEvent event) {
+                disableButtons(questionRow);
                 if (options.getValue().equals(thisInterval.getName())) {
                     questionRow.setStyle("-fx-background-color: green;");
                     manager.add(firstNote.getNote(), secondNote.getNote(), true);
