19de2dc5663bbec3a15a7d384506fbe2af1caf44
Sarah Jackson <svj13@uclive.ac.nz>
2016-04-13 14:54:51 +1200

    Musical Term
    Dom and I peer programmed to fix the case insenstivitiy.
    
    We also made musical term an array object in the dsl cup -> we made it a non terminal so if the user tries to add a musical term to the hashmap that requires more than 1 word, it will process correctly
    
    story #111

--- src/main/java/seng302/command/MusicalTerm.java
+++ src/main/java/seng302/command/MusicalTerm.java
@@ -4,6 +4,7 @@ package seng302.command;
  * Created by Sarah on 3/04/2016.
  */
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import seng302.Environment;
@@ -20,16 +21,32 @@ public class MusicalTerm implements Command {
      */
     public MusicalTerm () { this.isSetter = false; }
 
-    public MusicalTerm(String musicalTerm) {
+    public MusicalTerm(ArrayList<String> musicalTermList) {
         IntiailizeHashMap();
-        this.musicalTerm = musicalTerm.toLowerCase(); //changes input to lower case
-        //this.musicalTerm.toLowerCase();
+        Integer musicalTermWords = musicalTermList.size(); //an array to handle the "meaning of"
+        //input if there is more than one word (more than one argument)
+
+        //iterates through the musicalTermList to ensure all of the arguments are case insensitive
+        for (int i = 0; i < musicalTermWords; i++)
+        {
+            if (i == 0)
+            {
+                this.musicalTerm = musicalTermList.get(i).toLowerCase();
+            }
+            else
+            {
+                this.musicalTerm = this.musicalTerm.concat(" " + musicalTermList.get(i).toLowerCase());
+            }
+        }
+
         this.isSetter = true; //The function has been given an input
 
+        //checks to see if the definition exists in the hashmap
         if (MusicalTermsMap.get(this.musicalTerm) != null) {
             this.result = MusicalTermsMap.get(musicalTerm);
         }
 
+        //if a given term is not in the hash map, it will return an error message to the user
         if (MusicalTermsMap.get(this.musicalTerm) == null) {
             this.result = String.format("%s is not recognised as a musical term.",
                     this.musicalTerm);
@@ -40,7 +57,11 @@ public class MusicalTerm implements Command {
     public void IntiailizeHashMap() {
         this.MusicalTermsMap = new HashMap<String, String>();
 
+
+        //preset definitions for testing
         MusicalTermsMap.put("lento", "Origin: Italian \nCategory: Tempo \nDefinition: The speed at" +
+                " which a passage of music is or should be played");
+        MusicalTermsMap.put("a b", "Origin: Italian \nCategory: Tempo \nDefinition: The speed at" +
                 " which a passage of music is or should be played");
     }
 
