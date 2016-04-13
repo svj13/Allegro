830590ea33d50900ef3304e21ae2682b2c5b5dc3
Sarah Jackson <svj13@uclive.ac.nz>
2016-04-13 14:01:02 +1200

    Musical Term
    Dom and I peer programmed to fix the error handling messages. We did this by removing the try catch
    
    story #111

--- src/main/java/seng302/command/MusicalTerm.java
+++ src/main/java/seng302/command/MusicalTerm.java
@@ -25,21 +25,23 @@ public class MusicalTerm implements Command {
         this.musicalTerm = musicalTerm.toLowerCase(); //changes input to lower case
         //this.musicalTerm.toLowerCase();
         this.isSetter = true; //The function has been given an input
-        try {
-            if (MusicalTermsMap.get(this.musicalTerm) != null) {
-                this.result = MusicalTermsMap.get(musicalTerm);
-            }
-        } catch (Exception e) {
+
+        if (MusicalTermsMap.get(this.musicalTerm) != null) {
+            this.result = MusicalTermsMap.get(musicalTerm);
+        }
+
+        if (MusicalTermsMap.get(this.musicalTerm) == null) {
             this.result = String.format("%s is not recognised as a musical term.",
                     this.musicalTerm);
         }
+
     }
 
     public void IntiailizeHashMap() {
         this.MusicalTermsMap = new HashMap<String, String>();
 
         MusicalTermsMap.put("lento", "Origin: Italian \nCategory: Tempo \nDefinition: The speed at" +
-                "which a passage of music is or should be played");
+                " which a passage of music is or should be played");
     }
 
     public void execute(Environment env) {
