30f28e4cf8335611b0a9382df3088e51c050ccdf
Emily Price <erp35@uclive.ac.nz>
2016-04-14 17:08:48 +1200

    Added tests for the interval static lookup methods.
    Story: 110

---
+++ src/test/java/seng302/data/IntervalTest.java
@@ -0,0 +1,46 @@
+package seng302.data;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Emily on 11/04/16.
+ */
+public class IntervalTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+    // Tests that the lookup by name method gets the correct object.
+    @Test
+    public void testLookupByName() throws Exception {
+        assertEquals(0, Interval.lookupByName("unison").getSemitones());
+        assertEquals("unison", Interval.lookupByName("unison").getName());
+        assertEquals(2, Interval.lookupByName("major second").getSemitones());
+        assertEquals("major second", Interval.lookupByName("major second").getName());
+        assertEquals(12, Interval.lookupByName("perfect octave").getSemitones());
+        assertEquals("perfect octave", Interval.lookupByName("perfect octave").getName());
+    }
+
+    // Tests that the lookup by semitone method gets the correct object.
+    @Test
+    public void testLookupBySemitones() throws Exception {
+        assertEquals(0, Interval.lookupBySemitones(0).getSemitones());
+        assertEquals("unison", Interval.lookupBySemitones(0).getName());
+        assertEquals(2, Interval.lookupBySemitones(2).getSemitones());
+        assertEquals("major second", Interval.lookupBySemitones(2).getName());
+        assertEquals(12, Interval.lookupBySemitones(12).getSemitones());
+        assertEquals("perfect octave", Interval.lookupBySemitones(12).getName());
+    }
+
+
+
+
+}
