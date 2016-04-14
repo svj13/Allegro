87e072e8cd11d0da1492f94837864731c058068b
Emily Price <erp35@uclive.ac.nz>
2016-04-14 17:32:27 +1200

    Wrote failing tests for the interval command class.
    Story: 108

---
+++ src/test/java/seng302/command/IntervalCommandTest.java
@@ -0,0 +1,38 @@
+package seng302.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import seng302.Environment;
+import seng302.utility.TranscriptManager;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IntervalCommandTest {
+
+    private Environment env;
+    @Mock
+    private TranscriptManager transcriptManager;
+
+    @Before
+    public void setUp() throws Exception {
+        env = new Environment();
+        env.setTranscriptManager(transcriptManager);
+    }
+
+    @Test
+    public void setsCorrectSemitoneResult() {
+        new IntervalCommand("perfect octave").execute(env);
+        verify(transcriptManager).setResult("8");
+    }
+
+    @Test
+    public void setsCorrectErrorResult() {
+        new IntervalCommand("blah").execute(env);
+        verify(transcriptManager).setResult("[ERROR] Unknown interval: blah");
+    }
+}
