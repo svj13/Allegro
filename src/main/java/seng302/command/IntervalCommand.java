8239a43b94d335b5d11e36d895d4fc505d73e07e
Emily Price <erp35@uclive.ac.nz>
2016-04-15 23:00:20 +1200

    Implemented the logic for getting a note provided tonic and interval.
    Story: 108

--- src/main/java/seng302/command/IntervalCommand.java
+++ src/main/java/seng302/command/IntervalCommand.java
@@ -17,23 +17,41 @@ import seng302.data.Note;
 
 
 public class IntervalCommand implements Command {
-    String search;
-    String type;
+    String intervalName;
+    String tonic;
     String outputType;
     private boolean octaveSpecified;
     private Note note;
-    private char[] letters;
 
     public IntervalCommand(String intervalName) {
-        search = intervalName;
+        this.intervalName = intervalName;
+        this.outputType = "semitones";
     }
 
+    public IntervalCommand(String intervalName, String tonic) {
+        this.intervalName = intervalName;
+        this.tonic = tonic;
+        this.outputType = "note";
+    }
+
+
     public void execute(Environment env) {
-        //This section of code gets the number of semitones in a given interval
-        try {
-            env.getTranscriptManager().setResult((Integer.toString(Interval.lookupByName(search).getSemitones())));
-        } catch (Exception e) {
-            env.error("Unknown interval: " + search);
+        if (outputType.equals("semitones")) {
+            //This section of code gets the number of semitones in a given interval
+            try {
+                env.getTranscriptManager().setResult((Integer.toString(Interval.lookupByName(intervalName).getSemitones())));
+            } catch (Exception e) {
+                env.error("Unknown interval: " + intervalName);
+            }
+        } else if (outputType.equals("note")) {
+            //First, search for the note
+            note = Note.lookup(tonic);
+
+            //Second, get the number of semitones
+            int numSemitones = Interval.lookupByName(intervalName).getSemitones();
+
+            note = note.semitoneUp(numSemitones);
+            env.getTranscriptManager().setResult(note.getNote());
         }
 
     }
