32fcfe22109bf91cd4a118d12d3d33d683980dfc
Emily Price <erp35@uclive.ac.nz>
2016-04-15 16:12:24 +1200

    Javadoc for play note command.
    Story: 105

--- src/main/java/seng302/command/PlayNote.java
+++ src/main/java/seng302/command/PlayNote.java
@@ -5,29 +5,43 @@ import seng302.data.Note;
 import seng302.utility.OctaveUtil;
 
 /**
- * Plays a note using the midi library
+ * Plays a note using the midi library, given a note/midi value, and an optional duration.
  */
 public class PlayNote implements Command {
 
     private String note;
     private String duration;
 
+    /**
+     * Constructor for PlayNote
+     * @param note Either the note or midi representation of note to be played
+     */
     public PlayNote(String note) {
         this.note = note;
     }
 
+    /**
+     * Constructor for PlayNote with custom duration
+     * @param note Either the note or midi representation of note to be played
+     * @param duration A custom duration to play the note for
+     */
     public PlayNote(String note, String duration) {
         this.note = note;
         this.duration = duration;
     }
 
-
+    /**
+     * Uses the music player class to play a given note.
+     * Displays errors if the given note or duration is invalid.
+     * @param env The environment in which to function.
+     */
     public void execute(Environment env) {
         try {
             Note playNote = Note.lookup(OctaveUtil.addDefaultOctave(note));
             if (this.duration == null) {
                 env.getPlayer().playNote(playNote);
             } else {
+                // The duration has been specified by the user
                 try {
                     int playDuration = Integer.parseInt(duration);
                     if (playDuration <= 0) {
@@ -35,10 +49,12 @@ public class PlayNote implements Command {
                     }
                     env.getPlayer().playNote(playNote, playDuration);
                 } catch (Exception e) {
+                    // Catches invalid durations
                     env.error("Invalid duration " + duration);
                 }
             }
         } catch (Exception e) {
+            // Catches invalid notes
             env.error("\'" + note + "\'" + " is not a valid note.");
         }
 
