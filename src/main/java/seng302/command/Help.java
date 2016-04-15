7f0701d64c06aaaa0184112d4b04916383584f43
Sarah Jackson <svj13@uclive.ac.nz>
2016-04-16 11:07:11 +1200

    Musical Term help definitions
    Added the appropriate definitions to the help command for "meaning of" and "add musical term"
    
    story #111

--- src/main/java/seng302/command/Help.java
+++ src/main/java/seng302/command/Help.java
@@ -10,10 +10,16 @@ public class Help implements Command {
 
     public void execute(Environment env) {
         env.getTranscriptManager().setResult("" +
+                "add musical term: when followed by a musical term in the format of 'name; origin; " +
+                        "category; tempo', will add the musical term to the Musical Term " +
+                        "dictionary \n" +
                 "crotchet duration : returns the duration of a crotchet in seconds at a given tempo" +
                 " speed in BPM \n" +
                 "force set tempo : when followed by a tempo, it will set the given tempo, even if it" +
                 " is outside of the appropriate range \n" +
+                "meaning of: when followed by a musical term, will display the defintion of that" +
+                " term in the transcript manager. If it does not exist, it will display that it is" +
+                " not an existing musical term in the Musical Term dictionary \n" +
                 "midi : when followed by a valid note, it will return its corresponding midi number" +
                 " within the range of 0-127. If an octave not specified to the note, it will default " +
                 "to octave 4. e.g. 'midi' C4 will return 60. It is not case sensitive \n" +
