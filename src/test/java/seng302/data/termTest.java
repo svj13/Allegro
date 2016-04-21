8104f180fab04ecd28b9e1e5005dbd95ee7bc95e
Joseph Weaver <jmw280@uclive.ac.nz>
2016-04-21 15:14:55 +1200

    Tested the Tutor class and the term class I needed to add getters to the tutorManager class for some of the testing
    story: 114

---
+++ src/test/java/seng302/data/termTest.java
@@ -0,0 +1,24 @@
+package seng302.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TermTest {
+    Term term1;
+
+    @Before
+    public void setUp() throws Exception {
+        term1 = new Term("name", "category", "origin", "definition");
+    }
+
+    @Test
+    public void testVariableStoredCorrectly() throws Exception {
+        assertEquals("name", term1.getMusicalTermName());
+        assertEquals("category", term1.getMusicalTermCategory());
+        assertEquals("origin", term1.getMusicalTermOrigin());
+    }
+
+}
