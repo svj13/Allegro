4776af742359e4cb489e56ee633fd0f6124523d4
Isabelle Taylor <iet11@uclive.ac.nz>
2016-04-30 16:10:55 +1200

    Added method to generate random scale. Story: 118

--- src/main/java/seng302/gui/ScaleRecognitionTutorController.java
+++ src/main/java/seng302/gui/ScaleRecognitionTutorController.java
@@ -1,11 +1,16 @@
 package seng302.gui;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.StringJoiner;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import seng302.Environment;
+import seng302.data.Note;
 
 /**
  * Controller class for the scale recognition tutor.
@@ -20,13 +25,32 @@ public class ScaleRecognitionTutorController extends TutorController {
     @FXML
     Button btnGo;
 
+    private Random rand;
+
     @FXML
     private void goAction(ActionEvent event) {
 
     }
 
+
     public void create(Environment env) {
         super.create(env);
+        rand = new Random();
+    }
+
+    public ArrayList<Note> getRandomScale() {
+        Note startNote = Note.lookup(Integer.toString(rand.nextInt(11) + 60));
+        int type = rand.nextInt(1);
+        String scaleType;
+        if (type == 0) {
+            scaleType = "major";
+        } else {
+            scaleType = "minor";
+        }
+
+        // Add # octaves and up/down selection here.
+        ArrayList<Note> scale = startNote.getOctaveScale(scaleType, 1, true);
+        return scale;
     }
 
 
