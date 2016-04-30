017f8e3d76426799c47d3f2afdcbb3ca9cdbaf3f
Emily Price <erp35@uclive.ac.nz>
2016-04-30 15:59:14 +1200

    Sets up scroll panes, go button, and labels.
    Story: 118

--- src/main/java/seng302/gui/ScaleRecognitionTutorController.java
+++ src/main/java/seng302/gui/ScaleRecognitionTutorController.java
@@ -1,13 +1,33 @@
 package seng302.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import seng302.Environment;
 
 /**
  * Controller class for the scale recognition tutor.
  */
 public class ScaleRecognitionTutorController extends TutorController {
+    @FXML
+    HBox settings;
+
+    @FXML
+    AnchorPane scaleTutorAnchor;
+
+    @FXML
+    Button btnGo;
+
+    @FXML
+    private void goAction(ActionEvent event) {
+
+    }
 
     public void create(Environment env) {
         super.create(env);
     }
+
+
 }
