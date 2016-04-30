968f70508c938afcdb722ac440302c830466968d
Emily Price <erp35@uclive.ac.nz>
2016-04-30 17:29:08 +1200

    On go button press, generates a single question with functional play and semi-functional skip buttons.
    Story: 118

--- src/main/java/seng302/gui/ScaleRecognitionTutorController.java
+++ src/main/java/seng302/gui/ScaleRecognitionTutorController.java
@@ -5,10 +5,15 @@ import java.util.Random;
 import java.util.StringJoiner;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.util.Pair;
 import seng302.Environment;
 import seng302.data.Note;
 
@@ -29,7 +34,20 @@ public class ScaleRecognitionTutorController extends TutorController {
 
     @FXML
     private void goAction(ActionEvent event) {
+        paneQuestions.setVisible(true);
+        paneResults.setVisible(false);
+        questionRows.getChildren().clear();
 
+        int type = rand.nextInt(1);
+        String scaleType;
+        if (type == 0) {
+            scaleType = "major";
+        } else {
+            scaleType = "minor";
+        }
+        HBox questionRow = generateQuestionPane(getRandomScale(scaleType), scaleType);
+        questionRows.getChildren().add(questionRow);
+        questionRows.setMargin(questionRow, new Insets(10, 10, 10, 10));
     }
 
 
@@ -38,19 +56,53 @@ public class ScaleRecognitionTutorController extends TutorController {
         rand = new Random();
     }
 
-    public ArrayList<Note> getRandomScale() {
+    public ArrayList<Note> getRandomScale(String scaleType) {
         Note startNote = Note.lookup(Integer.toString(rand.nextInt(11) + 60));
-        int type = rand.nextInt(1);
-        String scaleType;
-        if (type == 0) {
-            scaleType = "major";
-        } else {
-            scaleType = "minor";
-        }
 
         // Add # octaves and up/down selection here.
         ArrayList<Note> scale = startNote.getOctaveScale(scaleType, 1, true);
         return scale;
+    }
+
+    /**
+     * Creates a GUI pane for a single question
+     * @param scale The array list of notes to be played
+     * @param scaleType Whether it is a major or minor scale
+     * @return
+     */
+    public HBox generateQuestionPane(final ArrayList<Note> scale, String scaleType) {
+        final HBox questionRow = new HBox();
+        formatQuestionRow(questionRow);
+
+
+        Button play = new Button();
+        Image imagePlay = new Image(getClass().getResourceAsStream("/images/play-icon.png"), 20, 20, true, true);
+        play.setGraphic(new ImageView(imagePlay));
+        play.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                //Play the scale
+                env.getPlayer().playNotes(scale);
+            }
+        });
+
+        Button major = new Button("Major");
+        Button minor = new Button("Minor");
+
+        Button skip = new Button("Skip");
+        Image imageSkip = new Image(getClass().getResourceAsStream("/images/right-arrow.png"), 20, 20, true, true);
+        skip.setGraphic(new ImageView(imageSkip));
+        skip.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                // Disables only input buttons
+                disableButtons(questionRow, 1, 4);
+                formatSkippedQuestion(questionRow);
+            }
+        });
+
+        questionRow.getChildren().addAll(play, major, minor, skip);
+
+        questionRow.prefWidthProperty().bind(paneQuestions.prefWidthProperty());
+        return questionRow;
     }
 
 
