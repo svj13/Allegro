687962c1f778fcd5f10d53d55eb32047604a124e
Emily Price <erp35@uclive.ac.nz>
2016-05-20 11:34:12 +1200

    JUnit tests for getting the notes of a scale's key signature.
    Story: 292

---
+++ src/test/java/seng302/command/KeySignatureCommandTest.java
@@ -0,0 +1,57 @@
+package seng302.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashMap;
+
+import seng302.Environment;
+import seng302.managers.TranscriptManager;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by emily on 20/05/16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class KeySignatureCommandTest {
+
+    private Environment env;
+    @Mock
+    private TranscriptManager transcriptManager;
+
+    @Before
+    public void setUp() throws Exception {
+        env = new Environment();
+        env.setTranscriptManager(transcriptManager);
+    }
+
+
+    @Test
+    public void testKeySigFetchNotes() {
+        HashMap<String, String> scale = new HashMap<String, String>();
+        scale.put("scale_type", "minor");
+        scale.put("note", "C");
+        new KeySignatureCommand(scale, "notes").execute(env);
+        verify(transcriptManager).setResult("Bb, Eb, Ab");
+
+        scale.clear();
+        scale.put("scale_type", "major");
+        scale.put("note", "C");
+        new KeySignatureCommand(scale, "notes").execute(env);
+        verify(transcriptManager).setResult("C major has no key signatures");
+
+        scale.clear();
+        scale.put("scale_type", "major");
+        scale.put("note", "C#");
+        new KeySignatureCommand(scale, "notes").execute(env);
+        verify(transcriptManager).setResult("F#, C#, G#, D#, A#, E#, B#");
+
+
+    }
+
+
+}
