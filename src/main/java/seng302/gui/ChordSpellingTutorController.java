afa4471ecd38f0ef4d82867b67c396dd8686dbcd
Emily Price <erp35@uclive.ac.nz>
2016-07-02 16:22:23 +1200

    Implements a new, basic tutor pane with an empty controller.
    Story: 298

---
+++ src/main/java/seng302/gui/ChordSpellingTutorController.java
@@ -0,0 +1,20 @@
+package seng302.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+/**
+ * Created by emily on 2/07/16.
+ */
+public class ChordSpellingTutorController {
+    @FXML
+    HBox settings;
+
+    @FXML
+    AnchorPane chordSpellingAnchor;
+
+    @FXML
+    Button btnGo;
+}
