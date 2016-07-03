cb79b5a0f1c52b74578874f21b83a08a9663bfea
Emily Price <erp35@uclive.ac.nz>
2016-07-03 20:13:23 +1200

    Implements the generation logic of the Type One question (that being, a name is shown and the notes should be input). Does not yet generate an appropriate question pane, but the beginning logic is there.
    Story: 298

--- src/main/java/seng302/gui/ChordSpellingTutorController.java
+++ src/main/java/seng302/gui/ChordSpellingTutorController.java
@@ -1,13 +1,21 @@
 package seng302.gui;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.util.Pair;
 import seng302.Environment;
+import seng302.data.Note;
+import seng302.utility.TutorRecord;
+import seng302.utility.musicNotation.ChordUtil;
+import seng302.utility.musicNotation.OctaveUtil;
 
 /**
  * Created by emily on 2/07/16.
@@ -28,6 +36,8 @@ public class ChordSpellingTutorController extends TutorController {
     @FXML
     Button btnGo;
 
+    private Random rand;
+
     public void create(Environment env) {
         super.create(env);
         initialiseQuestionSelector();
@@ -35,17 +45,88 @@ public class ChordSpellingTutorController extends TutorController {
         chordTypes.getSelectionModel().selectFirst();
         numEnharmonics.getItems().addAll("only one", "all");
         numEnharmonics.getSelectionModel().selectFirst();
+        rand = new Random();
     }
 
     @FXML
     private void goAction(ActionEvent event) {
+        record = new TutorRecord();
+        paneQuestions.setVisible(true);
+        paneResults.setVisible(false);
+        manager.resetEverything();
+        manager.questions = selectedQuestions;
+
+        questionRows.getChildren().clear();
+        for (int i = 0; i < manager.questions; i++) {
+            HBox questionRow = setUpQuestion();
+            questionRows.getChildren().add(questionRow);
+            questionRows.setMargin(questionRow, new Insets(10, 10, 10, 10));
+        }
     }
 
+    /**
+     * Prepares a new question
+     *
+     * @return a question pane containing the question information
+     */
+    public HBox setUpQuestion() {
+        int type = rand.nextInt(2);
+        if (type == 0) {
+            //First type of question
+            int majorOrMinor = rand.nextInt(2);
+            String chordType;
+            if (majorOrMinor == 0) {
+                chordType = "major";
+            } else {
+                chordType = "minor";
+            }
 
+            boolean validChord = false;
+            String chordName = "";
+            ArrayList<Note> chordNotes = null;
+
+            while (!validChord) {
+                Note startNote = getRandomNote();
+                if (ChordUtil.getChord(startNote, chordType) != null) {
+                    validChord = true;
+                    chordNotes = ChordUtil.getChord(startNote, chordType);
+                    chordName = OctaveUtil.removeOctaveSpecifier(startNote.getNote()) + " " + chordType;
+
+                }
+            }
+
+            return generateQuestionA(chordName, chordNotes);
+        } else {
+            //Second type of question
+            return generateQuestionB();
+        }
+
+    }
+
+    /**
+     * Generates a note in the octave of middle C
+     *
+     * @return the random note
+     */
+    public Note getRandomNote() {
+        return Note.lookup(Integer.toString(rand.nextInt(11) + 60));
+    }
 
     @Override
     HBox generateQuestionPane(Pair data) {
         return null;
+    }
+
+    HBox generateQuestionA(String chordName, ArrayList<Note> chordNotes) {
+        System.out.println("Given the chord name " + chordName + ", the expected notes are");
+        for (Note note : chordNotes) {
+            System.out.println(OctaveUtil.removeOctaveSpecifier(note.getNote()));
+        }
+        return new HBox();
+    }
+
+    HBox generateQuestionB() {
+        return new HBox();
     }
 
     @Override
