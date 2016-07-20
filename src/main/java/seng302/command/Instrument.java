68ea6a2f2f9ab7753bd65956421bbe385fcab41d
Emily Price <erp35@uclive.ac.nz>
2016-07-20 15:21:45 +1200

    Creates the blank class skeleton for the Instrument commands.
    Story: 452

---
+++ src/main/java/seng302/command/Instrument.java
@@ -0,0 +1,21 @@
+package seng302.command;
+
+import seng302.Environment;
+
+/**
+ * Instrument command class includes logic for getting and setting the current instrument.
+ */
+public class Instrument implements Command {
+
+    /**
+     * Instrument Command Constructor
+     */
+    public Instrument() {
+
+    }
+
+    @Override
+    public void execute(Environment env) {
+
+    }
+}
