f0310389ac37582399dc0a313597dd07120832a2
Emily Price <erp35@uclive.ac.nz>
2016-07-20 15:28:23 +1200

    Further implements the blank class skeleton for the Instrument commands.
    Story: 452

--- src/main/java/seng302/command/Instrument.java
+++ src/main/java/seng302/command/Instrument.java
@@ -9,8 +9,20 @@ public class Instrument implements Command {
 
     /**
      * Instrument Command Constructor
+     * This constructor will be used for setting the instrument.
+     * @param instrument the ID number of the selected instrument (if applicable)
      */
-    public Instrument() {
+    public Instrument(int instrument) {
+
+    }
+
+    /**
+     * Instrument Command Constructor This constructor will be used for displaying either the
+     * current instrument, or all available instruments.
+     *
+     * @param type Whether we are displaying the current instrument, or all available instruments.
+     */
+    public Instrument(String type) {
 
     }
 
