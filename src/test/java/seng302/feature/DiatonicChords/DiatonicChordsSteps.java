801a69000bc34d8a8d71d44288f0deb89167b5ee
Isabelle Taylor <iet11@uclive.ac.nz>
2016-07-29 15:15:46 +1200

    Added cucumber tests for function of command. Story: 450

--- src/test/java/seng302/feature/DiatonicChords/DiatonicChordsSteps.java
+++ src/test/java/seng302/feature/DiatonicChords/DiatonicChordsSteps.java
@@ -30,9 +30,8 @@ public class DiatonicChordsSteps {
         result = env.getTranscriptManager().getTranscriptTuples().get(0).getResult();
     }
 
-
     @When("I type the command 'chord function (I|II|III|IV|V|VI|VII) (.+)'")
-    public void executeCommand(final String romanNumeral, final String key) {
+    public void executeChordFunctionCommand(final String romanNumeral, final String key) {
         env.getExecutor().executeCommand("chord function " + romanNumeral + " " + key);
         result = env.getTranscriptManager().getTranscriptTuples().get(0).getResult();
     }
@@ -40,5 +39,11 @@ public class DiatonicChordsSteps {
     @Then("The following is printed to the transcript pane - (.+)")
     public void verifyResult(final String quality) {
         assertThat(result, equalTo(quality));
+    }
+
+    @When("^I type the command 'function of (.+7th) (.+)'$")
+    public void executeFunctionCommand(final String chord, final String key) {
+        env.getExecutor().executeCommand("function of " + chord + " " + key);
+        result = env.getTranscriptManager().getTranscriptTuples().get(0).getResult();
     }
 }
