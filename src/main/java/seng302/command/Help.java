c4b473ddabc705c3258279d25ffb1cba3787090b
Emily Price <erp35@uclive.ac.nz>
2016-08-04 22:23:16 +1200

    The output of typing help (command name) now shows you ALL the available data!
    https://i.imgflip.com/18fb2d.jpg
    
    Story: 448

--- src/main/java/seng302/command/Help.java
+++ src/main/java/seng302/command/Help.java
@@ -15,17 +15,35 @@ public class Help implements Command {
         this.keyword = keyword;
     }
 
+    private String generateHelpResult(Command command) {
+        String output = "";
+        output += command.getCommandText() + ":\n";
+        output += command.getHelp() + "\n";
+        if (command.getParams().size() > 0) {
+            output += "Parameters: ";
+            output += String.join(", ", command.getParams());
+            output += "\n";
+        }
+        if (command.getOptions().size() > 0) {
+            output += "Options: ";
+            output += String.join(", ", command.getOptions());
+            output += "\n";
+        }
+        output += "Example: ";
+        output += command.getExample();
+        return output;
+    }
+
     public void execute(Environment env) {
         try {
             Command result = CommandData.keywordToCommand.get(keyword);
             if (result != null) {
-                env.getTranscriptManager().setResult(result.getHelp());
+                env.getTranscriptManager().setResult(generateHelpResult(result));
             } else {
                 env.getTranscriptManager().setResult("Showing DSL Reference");
                 env.getRootController().getTranscriptController().showDslRef();
             }
         } catch (Exception e) {
-            e.printStackTrace();
             env.getTranscriptManager().setResult("Showing DSL Reference");
             env.getRootController().getTranscriptController().showDslRef();
         }
