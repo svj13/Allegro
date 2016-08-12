c98025674c773514e94e7111bccd83c2d88bfe42
Joseph Weaver <jmw280@uclive.ac.nz>
2016-08-12 13:52:25 +1200

    Finished implementing DSL command, had issues on how to parse an interger through the dsl as well as a note
    story: 451

--- src/main/java/seng302/command/MajorModes.java
+++ src/main/java/seng302/command/MajorModes.java
@@ -1,6 +1,7 @@
 package seng302.command;
 
 import seng302.Environment;
+import seng302.data.Interval;
 import seng302.data.Note;
 
 import java.util.HashMap;
@@ -8,9 +9,9 @@ import java.util.HashMap;
 /**
  * Created by jmw280 on 10/08/16.
  */
-public class MajorMode implements Command {
+public class MajorModes implements Command {
 
-    private Note tonic;
+    private String tonic;
     private String mode;
     private Integer degree;
 
@@ -21,9 +22,9 @@ public class MajorMode implements Command {
 
 
 
-    private MajorMode(Note tonic, Integer degree ){
+    public MajorModes(String tonic, String degree ){
         this.tonic = tonic;
-        this.degree = degree;
+        this.degree = Integer.valueOf(degree);
 
         generateModes();
     }
@@ -43,7 +44,7 @@ public class MajorMode implements Command {
 
     public void getCorresponingScale(Note key, Integer degree){
         //next note = get next note in scale * degree
-        String outputString = note + modes.get(degree);
+        //String outputString = note + modes.get(degree);
 
 
 
