aa348b33958c9b148a0cbbe8c5f563a5b7785400
Emily Price <erp35@uclive.ac.nz>
2016-08-14 15:06:00 +1200

    Adds help information for the "parent of" and "mode of" major mode commands.
    Story: 451

--- src/main/java/seng302/command/MajorModes.java
+++ src/main/java/seng302/command/MajorModes.java
@@ -1,10 +1,11 @@
 package seng302.command;
 
 import seng302.Environment;
-import seng302.data.Interval;
 import seng302.data.Note;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by jmw280 on 10/08/16.
@@ -15,7 +16,9 @@ public class MajorModes implements Command {
     private String mode;
     private Integer degree;
     private String startNote;
-    private String type;
+    private String scaleType;
+
+    private String commandType;
 
     private String outputString;
 
@@ -25,6 +28,7 @@ public class MajorModes implements Command {
 
     // This one is used for mode of command
     public MajorModes(String tonic, String degree ){
+        this.commandType = "modeOf";
         this.tonic = tonic;
         this.degree = Integer.valueOf(degree);
 
@@ -33,8 +37,9 @@ public class MajorModes implements Command {
 
     // Used for finding parent of
     public MajorModes(HashMap<String, String> scale) {
+        this.commandType = "parentOf";
         this.startNote = scale.get("note");
-        this.type = scale.get("scale_type");
+        this.scaleType = scale.get("scale_type");
 
         //continue doing things here
 
@@ -61,28 +66,49 @@ public class MajorModes implements Command {
 
     }
 
-
-
-
-
-
-
     public void execute(Environment env){
 
     }
 
     @Override
     public String getHelp() {
-        return null;
+        if (commandType.equals("modeOf")) {
+            return "Given a starting note and a degree, provides the corresponding scale" +
+                    " of the given note and the major mode of the given degree.";
+        } else {
+            return "Given a note and a major mode name, displays the name of the corresponding " +
+                    "major scale.";
+        }
     }
 
     @Override
     public String getCommandText() {
-        return null;
+        if (commandType.equals("modeOf")) {
+            return "mode of";
+        } else {
+            return "parent of";
+        }
     }
 
     @Override
     public String getExample() {
-        return null;
+        if (commandType.equals("modeOf")) {
+            return "mode of C 2";
+        } else {
+            return "parent of A phrygian";
+        }
+    }
+
+    @Override
+    public List<String> getParams() {
+        List<String> params = new ArrayList<>();
+        if (commandType.equals("modeOf")) {
+            params.add("note");
+            params.add("1-7");
+        } else {
+            params.add("note");
+            params.add("major mode type");
+        }
+        return params;
     }
 }
