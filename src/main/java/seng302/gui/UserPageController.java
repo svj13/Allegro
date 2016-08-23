da055cca3fdf470d0eddf596c428cad599763260
Isabelle Taylor <iet11@uclive.ac.nz>
2016-08-23 18:48:39 +1200

    Changed graphs to use fxml settings. This created a bug that took a while to find where the data was not displaying. Eventually found a setting that caused it. Also made all axies invisible. And cleared data inbetween clicks to make every tutor display correctly. Story:458

--- src/main/java/seng302/gui/UserPageController.java
+++ src/main/java/seng302/gui/UserPageController.java
@@ -1,13 +1,12 @@
 package seng302.gui;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.SnapshotParameters;
-import javafx.scene.chart.*;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
@@ -31,10 +30,13 @@ public class UserPageController {
     AnchorPane contentPane;
 
     @FXML
-    AnchorPane graphPane;
+    VBox stats;
 
     @FXML
-    AnchorPane recentGraphPane;
+    StackedBarChart stackedBar;
+
+    @FXML
+    StackedBarChart recentBar;
 
     @FXML
     AnchorPane topPane;
@@ -47,9 +49,6 @@ public class UserPageController {
 
     @FXML
     PieChart pieChart;
-
-    @FXML
-    StackedBarChart stackChart;
 
     @FXML
     ImageView imageDP;
@@ -66,8 +65,7 @@ public class UserPageController {
     }
 
 
-
-    public void populateUserOptions(){
+    public void populateUserOptions() {
 
         ArrayList<String> options = new ArrayList<>();
         options.add("Musical Terms Tutor");
@@ -83,17 +81,19 @@ public class UserPageController {
         Image lockImg = new Image(getClass().getResourceAsStream("/images/lock.png"), 10, 10, false, false);
 
 
-        for(String option: options){
+        for (String option : options) {
             Button optionBtn;
-            if(option.equals("Scale Recognition Tutor")){
+            if (option.equals("Scale Recognition Tutor")) {
                 optionBtn = new Button(option, new ImageView(lockImg));
                 optionBtn.setDisable(true);
 
-            }else{
+            } else {
                 optionBtn = new Button(option);
                 optionBtn.setOnAction(event -> {
                     displayGraphs(option);
 
+                    //displayGraphInLine(option);
+                    //displayScoresOverTime(option);
                 });
 
             }
@@ -105,22 +105,9 @@ public class UserPageController {
 
     }
 
-    private void displayGraph(String tutor) {
-        Pair<Integer, Integer> correctIncorrect = new Pair<>(0, 0);
-        chartTitle.setText(tutor);
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-        if (tutor.equals("Pitch Comparison Tutor")) {
-            correctIncorrect = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("pitchTutor");
-        }
-
-        pieChartData.add(new PieChart.Data("Correct", correctIncorrect.getKey()));
-        pieChartData.add(new PieChart.Data("Incorrect", correctIncorrect.getValue()));
-        pieChart.dataProperty().setValue(pieChartData);
-
-
+    private void displayScoresOverTime(String tutor) {
 
     }
-
 
     /**
      * creates the most recent tutor record graph and the overall tutor record graph
@@ -129,103 +116,55 @@ public class UserPageController {
     private void displayGraphs(String tutor) {
         Pair<Integer, Integer> correctIncorrectRecent = new Pair<>(0, 0);
         Pair<Integer, Integer> correctIncorrectOverall = new Pair<>(0, 0);
-        chartTitle.setText(tutor + " total questions");
 
-        final CategoryAxis xAxisRecent = new CategoryAxis();
-        final NumberAxis yAxisRecent = new NumberAxis();
-
-        StackedBarChart<Number, String> recentChart = new StackedBarChart<>(yAxisRecent, xAxisRecent);
-        recentChart.setMaxWidth(351);
-        recentChart.setMaxHeight(85);
-        recentChart.setLegendVisible(false);
-        recentChart.setAlternativeColumnFillVisible(false);
-        recentChart.setAlternativeRowFillVisible(false);
-        recentChart.setHorizontalZeroLineVisible(false);
-        recentChart.setHorizontalGridLinesVisible(false);
-        recentChart.setVerticalGridLinesVisible(false);
-        recentChart.setVerticalZeroLineVisible(false);
-        xAxisRecent.setTickLabelsVisible(false);
-        yAxisRecent.setTickLabelsVisible(false);
-        xAxisRecent.setTickMarkVisible(false);
-        yAxisRecent.setTickMarkVisible(false);
-
-        xAxisRecent.setVisible(false);
-        yAxisRecent.setVisible(false);
-
-        final CategoryAxis xAxisOverall = new CategoryAxis();
-        final NumberAxis yAxisOverall = new NumberAxis();
-
-        StackedBarChart<Number, String> overallChart = new StackedBarChart<>(yAxisOverall, xAxisOverall);
-        overallChart.setMaxWidth(351);
-        overallChart.setMaxHeight(85);
-        overallChart.setLegendVisible(false);
-        overallChart.setAlternativeColumnFillVisible(false);
-        overallChart.setAlternativeRowFillVisible(false);
-        overallChart.setHorizontalZeroLineVisible(false);
-        overallChart.setHorizontalGridLinesVisible(false);
-        overallChart.setVerticalGridLinesVisible(false);
-        overallChart.setVerticalZeroLineVisible(false);
-        xAxisOverall.setTickLabelsVisible(false);
-        yAxisOverall.setTickLabelsVisible(false);
-        xAxisOverall.setTickMarkVisible(false);
-        yAxisOverall.setTickMarkVisible(false);
-
-        xAxisOverall.setVisible(false);
-        yAxisOverall.setVisible(false);
-
-
-        recentGraphPane.getChildren().add(recentChart);
-        graphPane.getChildren().add(overallChart);
-
-
-        XYChart.Series< Number, String> recentSeries1 = new XYChart.Series<>();
+        XYChart.Series<Number, String> recentSeries1 = new XYChart.Series<>();
         XYChart.Series<Number, String> recentSeries2 = new XYChart.Series<>();
 
-        XYChart.Series< Number, String> overallSeries1 = new XYChart.Series<>();
+        XYChart.Series<Number, String> overallSeries1 = new XYChart.Series<>();
         XYChart.Series<Number, String> overallSeries2 = new XYChart.Series<>();
 
 
         if (tutor.equals("Pitch Comparison Tutor")) {
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("pitchTutor");
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("pitchTutor");
-        }else if(tutor.equals("Interval Recognition Tutor")){
+        } else if (tutor.equals("Interval Recognition Tutor")) {
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("intervalTutor");
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("intervalTutor");
-        }else if(tutor.equals("Scale Recognition Tutor")){
+        } else if (tutor.equals("Scale Recognition Tutor")) {
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("scaleTutor");
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("scaleTutor");
-        }else if(tutor.equals("Musical Terms Tutor")){
+        } else if (tutor.equals("Musical Terms Tutor")) {
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("musicalTermTutor");
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("musicalTermTutor");
-        }else if(tutor.equals("Chord Recognition Tutor")){
+        } else if (tutor.equals("Chord Recognition Tutor")) {
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("chordTutor");
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("chordTutor");
-        }else if(tutor.equals("Chord Spelling Tutor")){
+        } else if (tutor.equals("Chord Spelling Tutor")) {
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("chordSpellingTutor");
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("chordSpellingTutor");
-        }else if(tutor.equals("Key Signature Tutor")){
+        } else if (tutor.equals("Key Signature Tutor")) {
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("keySignatureTutor");
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("keySignatureTutor");
-        }else if(tutor.equals("Diatonic Chord Tutor")){
+        } else if (tutor.equals("Diatonic Chord Tutor")) {
             correctIncorrectRecent = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getRecentTutorTotals("diatonicChordTutor");
             correctIncorrectOverall = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().tutorHandler.getTutorTotals("diatonicChordTutor");
         }
 
         recentSeries1.getData().add(new XYChart.Data<>(correctIncorrectRecent.getKey(), ""));
         recentSeries2.getData().add(new XYChart.Data<>(correctIncorrectRecent.getValue(), ""));
-        recentChart.getData().addAll(recentSeries1, recentSeries2);
+        recentBar.getData().clear();
+        recentBar.getData().addAll(recentSeries1, recentSeries2);
 
         overallSeries1.getData().add(new XYChart.Data<>(correctIncorrectOverall.getKey(), ""));
         overallSeries2.getData().add(new XYChart.Data<>(correctIncorrectOverall.getValue(), ""));
-        overallChart.getData().addAll(overallSeries1, overallSeries2);
+        stackedBar.getData().clear();
+        stackedBar.getData().addAll(overallSeries1, overallSeries2);
 
 
     }
 
-
     public void updateImage(){
         final Circle clip = new Circle(imageDP.getFitWidth()-50.0, imageDP.getFitHeight()-50.0, 100.0);
-
 
         imageDP.setImage(env.getUserHandler().getCurrentUser().getUserPicture());
 
@@ -245,9 +184,6 @@ public class UserPageController {
 
 
     }
-
-
-
 
 
 }
