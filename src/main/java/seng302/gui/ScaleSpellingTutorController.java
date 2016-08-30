090c457bf7c8fecbc140f16252a2209e06da657b
Emily Price <erp35@uclive.ac.nz>
2016-08-30 17:27:19 +1200

    Removes some unused FXML objects from scale spelling tutor (these are declared in the parent class)
    Story: 616

--- src/main/java/seng302/gui/ScaleSpellingTutorController.java
+++ src/main/java/seng302/gui/ScaleSpellingTutorController.java
@@ -18,11 +18,8 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Pair;
 import seng302.Environment;
@@ -36,41 +33,18 @@ public class ScaleSpellingTutorController extends TutorController {
     @FXML
     private HBox settings;
 
-    @FXML
-    private VBox resultsBox;
-
-    @FXML
-    private VBox questionRows;
-
-    @FXML
-    private HBox buttons;
-
-    @FXML
-    private Label questions;
-
-    @FXML
-    private ScrollPane paneResults;
 
     @FXML
     private HBox settingsBox;
 
-    @FXML
-    private Slider numQuestions;
 
     @FXML
     private AnchorPane chordSpellingAnchor;
 
-    @FXML
-    private Text resultsTitle;
 
     @FXML
     private Text scaleError;
 
-    @FXML
-    private ScrollPane paneQuestions;
-
-    @FXML
-    private Text resultsContent;
 
     @FXML
     private Button btnGo;
