8f9394a870845f2b0ca25e68746a8a4e44a4badc
Jonty Trombik <jat157@uclive.ac.nz>
2016-08-30 13:05:50 +1200

    Changed the UI picker screen to be JFOENIXED
    story:466

--- src/main/java/seng302/gui/UISkinnerController.java
+++ src/main/java/seng302/gui/UISkinnerController.java
@@ -1,24 +1,18 @@
 package seng302.gui;
+
+import com.jfoenix.controls.JFXColorPicker;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import org.controlsfx.control.PopOver;
 import seng302.Environment;
+import seng302.utility.ColourUtils;
+
 
 import java.io.*;
 import java.lang.Math;
 
-import java.lang.reflect.Array;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 
@@ -31,10 +25,13 @@ public class UISkinnerController {
     @FXML
     ColorPicker colourPicker;
 
+
     @FXML
-    void skinGui(ActionEvent event) {
-        skinNode();
-    }
+    private JFXColorPicker jfxColourPicker;
+
+
+
+
 
     // RGB colour values
     private String baseRGB;
@@ -49,6 +46,7 @@ public class UISkinnerController {
     private ArrayList<String> rules = new ArrayList<String>();
 
 
+
     /**
      * initialises parameters needed for the gui
      *
@@ -60,34 +58,33 @@ public class UISkinnerController {
         this.baseNode = node;
     }
 
-    /**
-     *  Returns a string of formatted rgb values that can be used to format the stylesheet.
-     * @param color Color to convert
-     * @return Returns a string of formatted rgb values that can be used to format the stylesheet.
-     */
-    private String toRGBString(Color color) {
-        int red = (int) (color.getRed()*255);
-        int blue = (int) (color.getBlue()*255);
-        int green = (int) (color.getGreen()*255);
-        return (String.format("rgb(%s, %s, %s)", red, green, blue));
+
+    @FXML
+    void changeColour(ActionEvent event) {
+        Color base = jfxColourPicker.getValue();
+        String baseRgb = ColourUtils.toRGBString(base);
+        Color comp_colour = ColourUtils.getComplementaryColourString(base);
+        setDarkerRGB(base);
+        setLighterRGB(base);
+        String complementary_rgb = ColourUtils.toRGBString(comp_colour);
+        String styleString = "";
+        String lighterOrDarker;
+        double luma = 0.2126 * (base.getRed() * 255) + 0.7152 * (base.getGreen() * 255) + 0.0722 * (base.getBlue() * 255);
+        if (luma < 126) {
+            lighterOrDarker = floatToRGBString(lighterRGB);
+        } else {
+            lighterOrDarker = floatToRGBString(darkerRGB);
+        }
+
+        generateStyleSheet(baseRgb, lighterOrDarker);
+
+        baseNode.getStylesheets().clear();
+        String filePath = "userstyle.css";
+        File f = new File(filePath);
+        baseNode.getStylesheets().clear();
+        baseNode.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
     }
 
-    /**
-     * Gets the complementary colour of the given colour
-     * @param color Color value
-     * @return Complementary Colour
-     */
-    private Color getComplementaryColourString(Color color) {
-        Color comp_color;
-        int red = (int) (color.getRed()*255);
-        int blue = (int) (color.getBlue()*255);
-        int green = (int) (color.getGreen()*255);
-        double newRed = Math.sqrt(Math.pow(255, 2)-Math.pow(red, 2)) / 255;
-        double newBlue = Math.sqrt(Math.pow(255, 2)-Math.pow(blue, 2)) / 255;
-        double newGreen = Math.sqrt(Math.pow(255, 2)-Math.pow(green, 2)) / 255;
-        comp_color = new Color(newRed, newBlue, newGreen, 1);
-        return comp_color;
-    }
 
 
     /**
@@ -95,11 +92,11 @@ public class UISkinnerController {
      */
     private void skinNode() {
         Color base = colourPicker.getValue();
-        String baseRgb = toRGBString(base);
-        Color comp_colour = getComplementaryColourString(base);
+        String baseRgb = ColourUtils.toRGBString(base);
+        Color comp_colour = ColourUtils.getComplementaryColourString(base);
         setDarkerRGB(base);
         setLighterRGB(base);
-        String complementary_rgb = toRGBString(comp_colour);
+        String complementary_rgb = ColourUtils.toRGBString(comp_colour);
         String styleString = "";
         String lighterOrDarker;
         double luma = 0.2126 * (base.getRed()*255) + 0.7152 * (base.getGreen()*255) + 0.0722 * (base.getBlue()*255);
@@ -177,7 +174,7 @@ public class UISkinnerController {
      * @param color Color vaue
      */
     private void setLighterRGB(Color color) {
-        float[] hsl = rgbToHsl(colorToRgb(color));
+        float[] hsl = ColourUtils.rgbToHsl(ColourUtils.colorToRgb(color));
         float lightness = hsl[2];
         double lCoefficient;
         if (lightness > 0.8) {
@@ -196,7 +193,7 @@ public class UISkinnerController {
             lighterHslArray[2] = (float) (hsl[2] * lCoefficient);
         }
 
-        float[] lighterRGBArray = hslToRgb(lighterHslArray);
+        float[] lighterRGBArray = ColourUtils.hslToRgb(lighterHslArray);
 
         lighterRGB = lighterRGBArray;
     }
@@ -206,7 +203,7 @@ public class UISkinnerController {
      * @param color Color value
      */
     private void setDarkerRGB(Color color) {
-        float[] hsl = rgbToHsl(colorToRgb(color));
+        float[] hsl = ColourUtils.rgbToHsl(ColourUtils.colorToRgb(color));
         float lightness = hsl[2];
         double lCoefficient;
         if (lightness > 0.8) {
@@ -222,135 +219,13 @@ public class UISkinnerController {
         darkerHslArray[2] = (float) (hsl[2] * lCoefficient);
 
 
-        float[] darkerRGBArray = hslToRgb(darkerHslArray);
+        float[] darkerRGBArray = ColourUtils.hslToRgb(darkerHslArray);
 
 
         darkerRGB = darkerRGBArray;
     }
 
-    /**
-     * converts a color to rgb values
-     * @param color Color value
-     * @return Float array of rgb values
-     */
-    private float[] colorToRgb(Color color) {
-        float[] rgbVals = new float[3];
-        rgbVals[0] = (float) (color.getRed());
-        rgbVals[1] = (float) (color.getGreen());
-        rgbVals[2] = (float) (color.getBlue());
 
-        return rgbVals;
-    }
-
-    /**
-     * converts rgb values to hsl values
-     * @param rgb Float array of rgb values
-     * @return Returns a float array of hsl values
-     */
-    private float[] rgbToHsl(float[] rgb) {
-        float r = rgb[0];
-        float g = rgb[1];
-        float b = rgb[2];
-
-        float max = Math.max(Math.max(r, g), b);
-        float min = Math.min(Math.min(r, g), b);
-        float h, s, l;
-        h = s = l = ((max+min)/(float)2);
-
-        if(max == min){
-            h = s = 0; // achromatic
-        }else{
-
-            float d = max - min;
-
-            if(l > (float)0.5) {s = d / (2 - max - min);}
-            else{s = d / (max + min);}
-
-            if (r == max) {
-                float tempVar = 0;
-                if (g < b) {tempVar = (float) 6;}
-                else {tempVar = (float) 0;}
-                h = (g - b) / d + tempVar;
-            }
-            else if (r == max) {
-                h = (b - r) / d + (float) 2;
-            }
-            else if (r == max) {
-                h = (r - g) / d + (float) 4;
-            }
-            h = h / (float) 6;
-        }
-
-        float[] hslVals = new float[3];
-        hslVals[0] = h;
-        hslVals[1] = s;
-        hslVals[2] = l;
-
-        return hslVals;
-    }
-
-    /**
-     * converts hsl colours to rgb colours
-     * @param hsl Float array of hsl values
-     * @return Float array of rgb values
-     */
-    private float[] hslToRgb(float[] hsl) {
-        float h = hsl[0];
-        float s = hsl[1];
-        float l = hsl[2];
-
-
-        float r, g, b;
-
-        //Implements formula
-        if(s == (float) 0){
-            r = g = b = l; // achromatic
-        }else{
-            float temp1 = 0;
-            if(l < (float) 0.5) {
-                temp1 = l * (1 + s);
-            } else {
-                temp1 = (l + s) - (l * s);
-            }
-            float temp2 = (float) 2 * l - temp1;
-            r = hueToRGB(temp2, temp1, h + (float) 1 / 3);
-            g = hueToRGB(temp2, temp1, h);
-            b = hueToRGB(temp2, temp1, h - (float) 1 / 3);
-        }
-
-        float[] rgbVals = new float[3];
-        rgbVals[0] = Math.round(r * 255);
-        rgbVals[1] = Math.round(g * 255);
-        rgbVals[2] = Math.round(b * 255);
-
-        return rgbVals;
-    }
-
-    /**
-     * helper for the hsl to rgb values. Implements part of algorithm to convert hsl value to rgb
-     * @param p temporary variable used to store part of the formula
-     * @param q temporary variable used to store part of the formula
-     * @param t temporary variable used to determine part of the formula, red, green, or blue - outcome.
-     * @return Float representation of specific rgb value
-     */
-    private float hueToRGB(float p, float q, float t) {
-        if(t < (float) 0) {
-            t += 1;
-        }
-        if(t > (float) 1) {
-            t -= 1;
-        }
-        if(t < (float) 1/6) {
-            return p + ((q - p) * (float) 6 * t);
-        }
-        if(t < (float) 1/2) {
-            return q;
-        }
-        if(t < (float) 2/3) {
-            return p + ((q - p) * ((float) 2/3 - t) * (float) 6);
-        }
-        return p;
-    }
 
     /**
      * Converts an array of floats to a formatted string.
---
+++ src/main/java/seng302/utility/ColourUtils.java
@@ -0,0 +1,174 @@
+package seng302.utility;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Created by Jonty on 30-Aug-16.
+ */
+public class ColourUtils {
+    /**
+     * Returns a string of formatted rgb values that can be used to format the stylesheet.
+     *
+     * @param color Color to convert
+     * @return Returns a string of formatted rgb values that can be used to format the stylesheet.
+     */
+    public static String toRGBString(Color color) {
+        int red = (int) (color.getRed() * 255);
+        int blue = (int) (color.getBlue() * 255);
+        int green = (int) (color.getGreen() * 255);
+        return (String.format("rgb(%s, %s, %s)", red, green, blue));
+    }
+
+    /**
+     * Gets the complementary colour of the given colour
+     *
+     * @param color Color value
+     * @return Complementary Colour
+     */
+    public static Color getComplementaryColourString(Color color) {
+        Color comp_color;
+        int red = (int) (color.getRed() * 255);
+        int blue = (int) (color.getBlue() * 255);
+        int green = (int) (color.getGreen() * 255);
+        double newRed = Math.sqrt(Math.pow(255, 2) - Math.pow(red, 2)) / 255;
+        double newBlue = Math.sqrt(Math.pow(255, 2) - Math.pow(blue, 2)) / 255;
+        double newGreen = Math.sqrt(Math.pow(255, 2) - Math.pow(green, 2)) / 255;
+        comp_color = new Color(newRed, newBlue, newGreen, 1);
+        return comp_color;
+    }
+
+
+    /**
+     * converts a color to rgb values
+     *
+     * @param color Color value
+     * @return Float array of rgb values
+     */
+    public static float[] colorToRgb(Color color) {
+        float[] rgbVals = new float[3];
+        rgbVals[0] = (float) (color.getRed());
+        rgbVals[1] = (float) (color.getGreen());
+        rgbVals[2] = (float) (color.getBlue());
+
+        return rgbVals;
+    }
+
+    /**
+     * converts rgb values to hsl values
+     *
+     * @param rgb Float array of rgb values
+     * @return Returns a float array of hsl values
+     */
+    public static float[] rgbToHsl(float[] rgb) {
+        float r = rgb[0];
+        float g = rgb[1];
+        float b = rgb[2];
+
+        float max = Math.max(Math.max(r, g), b);
+        float min = Math.min(Math.min(r, g), b);
+        float h, s, l;
+        h = s = l = ((max + min) / (float) 2);
+
+        if (max == min) {
+            h = s = 0; // achromatic
+        } else {
+
+            float d = max - min;
+
+            if (l > (float) 0.5) {
+                s = d / (2 - max - min);
+            } else {
+                s = d / (max + min);
+            }
+
+            if (r == max) {
+                float tempVar = 0;
+                if (g < b) {
+                    tempVar = (float) 6;
+                } else {
+                    tempVar = (float) 0;
+                }
+                h = (g - b) / d + tempVar;
+            } else if (r == max) {
+                h = (b - r) / d + (float) 2;
+            } else if (r == max) {
+                h = (r - g) / d + (float) 4;
+            }
+            h = h / (float) 6;
+        }
+
+        float[] hslVals = new float[3];
+        hslVals[0] = h;
+        hslVals[1] = s;
+        hslVals[2] = l;
+
+        return hslVals;
+    }
+
+    /**
+     * converts hsl colours to rgb colours
+     *
+     * @param hsl Float array of hsl values
+     * @return Float array of rgb values
+     */
+    public static float[] hslToRgb(float[] hsl) {
+        float h = hsl[0];
+        float s = hsl[1];
+        float l = hsl[2];
+
+
+        float r, g, b;
+
+        //Implements formula
+        if (s == (float) 0) {
+            r = g = b = l; // achromatic
+        } else {
+            float temp1 = 0;
+            if (l < (float) 0.5) {
+                temp1 = l * (1 + s);
+            } else {
+                temp1 = (l + s) - (l * s);
+            }
+            float temp2 = (float) 2 * l - temp1;
+            r = hueToRGB(temp2, temp1, h + (float) 1 / 3);
+            g = hueToRGB(temp2, temp1, h);
+            b = hueToRGB(temp2, temp1, h - (float) 1 / 3);
+        }
+
+        float[] rgbVals = new float[3];
+        rgbVals[0] = Math.round(r * 255);
+        rgbVals[1] = Math.round(g * 255);
+        rgbVals[2] = Math.round(b * 255);
+
+        return rgbVals;
+    }
+
+    /**
+     * helper for the hsl to rgb values. Implements part of algorithm to convert hsl value to rgb
+     *
+     * @param p temporary variable used to store part of the formula
+     * @param q temporary variable used to store part of the formula
+     * @param t temporary variable used to determine part of the formula, red, green, or blue - outcome.
+     * @return Float representation of specific rgb value
+     */
+    public static float hueToRGB(float p, float q, float t) {
+        if (t < (float) 0) {
+            t += 1;
+        }
+        if (t > (float) 1) {
+            t -= 1;
+        }
+        if (t < (float) 1 / 6) {
+            return p + ((q - p) * (float) 6 * t);
+        }
+        if (t < (float) 1 / 2) {
+            return q;
+        }
+        if (t < (float) 2 / 3) {
+            return p + ((q - p) * ((float) 2 / 3 - t) * (float) 6);
+        }
+        return p;
+    }
+
+
+}
