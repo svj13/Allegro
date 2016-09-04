57387f1a6af45eb7566c9969bb22ebcca320b107
Sarah Jackson <svj13@uclive.ac.nz>
2016-09-05 01:36:24 +1200

    Wrote out skeleton functions with their pseudocode. wrote out documentation
    story: 617

--- src/main/java/seng302/gui/StageMapController.java
+++ src/main/java/seng302/gui/StageMapController.java
@@ -2,6 +2,7 @@ package seng302.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import jdk.nashorn.internal.parser.JSONParser;
 import seng302.Environment;
 import javafx.scene.control.Button;
 
@@ -48,14 +49,17 @@ public class StageMapController {
     @FXML
     private Button intervalRecognitionButton;
 
-    //sets the current tutor so the tutor can be launched upon the button click
-    String currentTutor;
+    UserPageController userPageController; //creates an insance of user page controller so we can access its methods
 
-    UserPageController userPageController;
+    Environment env; //declare the environment
+
+    private JSONParser parser; //used to parser the tutor records
 
 
-    Environment env;
-
+    /**
+     * Initializes the environment and the user page controller instance
+     * @param env
+     */
     public void setEnvironment(Environment env) {
         this.env = env;
         userPageController = env.getUserPageController();
@@ -65,15 +69,57 @@ public class StageMapController {
     public StageMapController() {
 
 
+    }
+
+    /**
+     * Fetches tutor score file of tutor of interest and parses items of interest. Will terminate if it cannot
+     * source more than 5 entries
+     */
+    private void fetchTutorFile() {
+        JSONParser parser = new JSONParser();
+        boolean enoughEntries = false; //must be at least 5 entries for their to be a valid entry
+
+
+        if (enoughEntries) {
+            System.out.println("You need to attempt this tutor 5x consecutively with an " +
+                    "average score of 90% to proceed to the next tutor. Soz");
+        } else {
+
+        }
+
+
+        //project of interest
+        //tutor of interest
+
+        //-> follow brrm brrm path the the tutor record
+        //->parse into tree
 
 
     }
 
-    private void openTutorTab(String tutor) {
+
+    /**
+     * Takes the tutor score file, finds the scores of the last 5 entries and calculates the average over the last
+     * 5 scores
+     */
+    private void calculateAverageScore() {
+
+        //find last x scores
+        //calculate last x scores
+        //return average
+
+
+
 
     }
 
 
+
+
+    /*
+    //FXML methods below give actions to the buttons on the stage map. When a stage on the stage map is selected,
+    //the corresponding tutor will launch
+     */
     @FXML
     private void launchMusicalTermsTutor() {
         userPageController.setCurrentTutor("Musical Terms Tutor");
