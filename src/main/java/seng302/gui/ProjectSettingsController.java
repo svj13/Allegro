eb31f91461e6414c927ed36d9b48478b1c2389f5
Emily Price <erp35@uclive.ac.nz>
2016-09-15 11:26:06 +1200

    Moves mode and visualiser toggle from user settings to project settings page.
    Story: 640

--- src/main/java/seng302/gui/ProjectSettingsController.java
+++ src/main/java/seng302/gui/ProjectSettingsController.java
@@ -32,6 +32,12 @@ public class ProjectSettingsController {
     private JFXToggleButton modeToggle;
 
     @FXML
+    private JFXToggleButton visualiserToggle;
+
+    @FXML
+    private Label visualiserLabel;
+
+    @FXML
     private JFXComboBox instrumentSelector;
 
     @FXML
@@ -50,28 +56,11 @@ public class ProjectSettingsController {
         env.getRootController().setHeader("Project Settings");
         projectHandler = env.getUserHandler().getCurrentUser().getProjectHandler();
 
-        modeToggle.getStyleClass().remove(0);
 
-        try {
-            modeToggle.setSelected(projectHandler.getCurrentProject().getIsCompetitiveMode());
-        } catch (Exception e) {
-            // Default to competition mode
-            modeToggle.setSelected(true);
-        }
+        setupToggles();
 
-        tempoText.setText(Integer.toString(env.getPlayer().getTempo()));
-        tempoSlider.setValue(env.getPlayer().getTempo());
 
-        // The listener for the number of questions selected
-        tempoSlider.valueProperty().addListener((observable, newValue, oldValue) -> {
-            tempoText.setText(Integer.toString(newValue.intValue()));
-
-            if (newValue.intValue() >= 20 && newValue.intValue() <= 300) {
-                env.getPlayer().setTempo(newValue.intValue());
-                projectHandler.getCurrentProject().checkChanges("tempo");
-
-            }
-        });
+        setupTempoSlider();
 
 
         setupInstrumentSelector();
@@ -133,4 +122,96 @@ public class ProjectSettingsController {
             }
         });
     }
+
+    private void setupTempoSlider() {
+        tempoText.setText(Integer.toString(env.getPlayer().getTempo()));
+        tempoSlider.setValue(env.getPlayer().getTempo());
+
+        // The listener for the number of questions selected
+        tempoSlider.valueProperty().addListener((observable, newValue, oldValue) -> {
+            tempoText.setText(Integer.toString(newValue.intValue()));
+
+            if (newValue.intValue() >= 20 && newValue.intValue() <= 300) {
+                env.getPlayer().setTempo(newValue.intValue());
+                projectHandler.getCurrentProject().checkChanges("tempo");
+
+            }
+        });
+    }
+
+    private void setupToggles() {
+        modeToggle.getStyleClass().remove(0);
+        visualiserToggle.getStyleClass().remove(0);
+
+        try {
+            modeToggle.setSelected(projectHandler.getCurrentProject().getIsCompetitiveMode());
+        } catch (Exception e) {
+            // Default to competition mode
+            modeToggle.setSelected(true);
+        }
+
+
+        if (projectHandler.getCurrentProject().getIsCompetitiveMode()) {
+            // do not show the visualiser options
+            toggleShowHideVisualiser(false);
+        } else {
+            try {
+                boolean visualiserOn = projectHandler.getCurrentProject().getVisualiserOn();
+                visualiserToggle.setSelected(visualiserOn);
+                if (visualiserOn) {
+                    visualiserLabel.setText("Keyboard Visualiser ON");
+                } else {
+                    visualiserLabel.setText("Keyboard Visualiser OFF");
+                }
+            } catch (Exception e) {
+                // Default to off
+                visualiserToggle.setSelected(false);
+                visualiserLabel.setText("Keyboard Visualiser OFF");
+            }
+        }
+
+        visualiserToggle.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                projectHandler.getCurrentProject().setVisualiserOn(true);
+                env.getUserHandler().getCurrentUser().updateProperties();
+                env.getUserHandler().getCurrentUser().saveProperties();
+                visualiserLabel.setText("Keyboard Visualiser ON");
+
+            } else {
+                env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().setVisualiserOn(false);
+                env.getUserHandler().getCurrentUser().updateProperties();
+                env.getUserHandler().getCurrentUser().saveProperties();
+                visualiserLabel.setText("Keyboard Visualiser OFF");
+            }
+        });
+
+        modeToggle.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            Project currentProject = env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject();
+            if (newValue) {
+                // Competition Mode
+                currentProject.setIsCompetitiveMode(true);
+                toggleShowHideVisualiser(false);
+            } else {
+                // Practice mode
+                currentProject.setIsCompetitiveMode(false);
+                toggleShowHideVisualiser(true);
+            }
+        });
+
+    }
+
+    /**
+     * Updates the GUI to show or hide the visualiser toggle and label.
+     *
+     * @param isShow If false, hide the visualiser toggle and its label. Else, show the visualiser
+     *               and its toggle.
+     */
+    private void toggleShowHideVisualiser(boolean isShow) {
+        visualiserToggle.setVisible(isShow);
+        visualiserLabel.setVisible(isShow);
+        visualiserToggle.setManaged(isShow);
+        visualiserLabel.setManaged(isShow);
+    }
+
+
 }
