5be9b5562b8558a6c00cd021a4eb81c69cd4c2b1
Emily Price <erp35@uclive.ac.nz>
2016-09-15 00:45:51 +1200

    Adds an instrument selector to the project settings page.
    Story: 640

--- src/main/java/seng302/gui/ProjectSettingsController.java
+++ src/main/java/seng302/gui/ProjectSettingsController.java
@@ -1,14 +1,21 @@
 package seng302.gui;
 
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.midi.Instrument;
 
 import javafx.fxml.FXML;
 import seng302.Environment;
 import seng302.Users.Project;
 import seng302.Users.ProjectHandler;
 import seng302.command.Tempo;
+import seng302.utility.InstrumentUtility;
 
 /**
  * Controller class for the GUI used to change project-based settings.
@@ -23,6 +30,9 @@ public class ProjectSettingsController {
 
     @FXML
     private JFXToggleButton modeToggle;
+
+    @FXML
+    private JFXComboBox instrumentSelector;
 
     private Environment env;
 
@@ -52,6 +62,8 @@ public class ProjectSettingsController {
             new Tempo(Integer.toString(newValue.intValue()), false).execute(env);
             env.getUserHandler().getCurrentUser().getProjectHandler().getCurrentProject().checkChanges("tempo");
         });
+
+        setupInstrumentSelector();
     }
 
     @FXML
@@ -64,5 +76,24 @@ public class ProjectSettingsController {
             // Practice mode
             currentProject.setIsCompetitiveMode(false);
         }
+    }
+
+    private void setupInstrumentSelector() {
+        Instrument[] instruments = env.getPlayer().getAvailableInstruments();
+
+        List<String> textInstruments = new ArrayList<>();
+
+        for (Instrument instrument : instruments) {
+            textInstruments.add(instrument.getName());
+
+        }
+
+        instrumentSelector.getItems().setAll(textInstruments);
+        instrumentSelector.getSelectionModel().select(env.getPlayer().getInstrument().getName());
+
+        instrumentSelector.valueProperty().addListener((observable, oldValue, newValue) -> {
+            env.getPlayer().setInstrument(InstrumentUtility.getInstrumentByName((String) newValue, env));
+            projectHandler.getCurrentProject().checkChanges("instrument");
+        });
     }
 }
